6c7b57bd58acf824433243e75a5854b7b185a227
Viraj <virajdomadia32@gmail.com>
2023-11-02 12:31:39 +0530

    First Commit

---
+++ Pattern3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n, m;
+    cout<< "Enter number of rows : ";
+    cin>>n;
+    cout<<"Enter number of columns: ";
+    cin>>m;
+    for (int i = 1; i <= n; i++){
+        for(int j = 1; j<=m; j++){
+            if(i == 0 && i == 2){
+                cout<<"* ";
+            }
+            else{
+                if(j == 0 && j == 2)
+            }
+        }
+        cout<<endl;
+    }
+}
