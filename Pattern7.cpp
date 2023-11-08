6f7a6156ef274dcae7ab756bf87f76d362909315
Viraj <virajdomadia32@gmail.com>
2023-11-08 11:18:38 +0530

    Pattern7- Inverted half Numeric Pyramid

---
+++ Pattern7.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter number of rows: ";
+    cin>>n;
+    for(int i=n; i >= 1; i--){
+        for(int j=0; j<i; j++){
+            cout<< j+1;
+        }
+        cout<<endl;
+    }
+}
