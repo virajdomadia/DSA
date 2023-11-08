d6e459d6018280bfbc256c1db60140a0b8ea627a
Viraj <virajdomadia32@gmail.com>
2023-11-08 11:21:12 +0530

    Pattern8- Hollow Square

---
+++ Pattern8.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n, m;
+    cout<< "Enter number of rows : ";
+    cin>>n;
+    for (int i = 0; i < n; i++){
+        if(i == 0 || i == n-1){
+            for(int j = 0; j < n; j++){
+                cout<< "* ";
+            }
+        }
+        else{
+            cout<<"* ";
+            for(int k = 0; k < n-2; k++){
+                cout << "  ";
+            }
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
