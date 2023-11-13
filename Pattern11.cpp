5f5294884cdb8c0ea492c84e02aacab06c0adfa1
Viraj <virajdomadia32@gmail.com>
2023-11-13 12:39:19 +0530

    Pattern 11 - Full Pyramid

---
+++ Pattern11.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter number of Rows: ";
+    cin>>n;
+
+    for(int i = 0; i<n; i++){
+        for(int k = 0; k<n-i-1; k++){
+            cout<<"_";
+        }
+        for(int j = 0; j<i+1; j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
