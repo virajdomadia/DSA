f7fb2d75a658ac5fd54ed148d12b3dbeacd4447c
Viraj <virajdomadia32@gmail.com>
2023-11-13 12:54:14 +0530

    Pattern 12 - Inverted Full Pyramid

---
+++ Pattern12.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter Number of Rows: ";
+    cin>>n;
+
+    for(int i = 0; i<n; i++){
+        for(int k = 0; k<i; k++){
+            cout<<" ";
+        }
+        for(int j = 0; j<n-i; j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
