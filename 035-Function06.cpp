40793bae84ddeb51b1f6c25eb6d63efe6ed7a810
Viraj <virajdomadia32@gmail.com>
2023-11-26 12:00:08 +0530

    035) Function06 - Write a Function to display area of circle

---
+++ 035-Function06.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+float areaCircle(float r){
+    float area;
+    area = 3.14*r*r;
+    return area;
+}
+int main(){
+    float r;
+    cout<<"Enter the radius of circle: ";
+    cin>>r;
+    cout<<areaCircle(r);
+}
