618951cae5ac7d3214b789d196bfa323ab489d2b
Viraj <virajdomadia32@gmail.com>
2023-11-26 13:37:30 +0530

    039) Function10 - Print all Prime number from 1 to N

---
+++ 039-Function10.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+bool checkPrime(int n){
+    int i = 2;
+    for(i=2; i<n;i++){
+        if(n%i==0){
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(){
+    int n;
+    cout<<"Enter number: ";
+    cin>>n;
+    for(int i = 2; i<=n; i++){
+        bool isiPrime = checkPrime(i);
+        if(isiPrime){
+            cout<<i<<" ";
+        }
+    }
+}
