be87071188a40ddf7bae58acac39c469ca4d57ee
Viraj <virajdomadia32@gmail.com>
2023-11-26 12:31:29 +0530

    037) Function08 - Find the Factorial

---
+++ 037-Function08.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int factorial(int n){
+    int fact = 1;
+
+    for(int i = 1; i<=n; i++){
+        fact *= i;
+    }
+    return fact; 
+}
+
+int main(){
+    int n;
+    cout<<"Enter number: ";
+    cin>>n;
+
+    cout<<"Factorial of "<<n<<" is "<<factorial(n);
+}
