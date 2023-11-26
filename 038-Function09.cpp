f498bafbba9a22b331411e0140ad1707e1b33922
Viraj <virajdomadia32@gmail.com>
2023-11-26 13:08:21 +0530

    038) Function09 - Check Given Number Prime or Not

---
+++ 038-Function09.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+bool checkPrime(int n){
+    int i = 2;
+    for(i=2; i<n;i++){
+        if(n%i==0){
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(){
+    int n;
+    cout<<"Enter number: ";
+    cin>>n;
+    bool isPrime = checkPrime(n);
+    if(isPrime){
+        cout<<"Prime";
+    }
+    else{
+        cout<<"Not Prime";
+    }
+}
